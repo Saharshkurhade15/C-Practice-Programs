074a870ce141bc04d3b63f19718e979376ec3f66
SAHARSH VIJAYRAO KURHADE <95751747+saharshkurhade664@users.noreply.github.com>
2021-12-13 16:59:07 +0530

    Add files via upload

---
+++ gets,puts,getchar,putchar/getchar putchar prob.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	char c;
+	printf("\nEnter any character : ");
+	c=getchar();
+	printf("\nCharacter is : ");
+	putchar(c);
+	
+    return 0;
+}
+
---
+++ gets,puts,getchar,putchar/gets puts prob.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	char string[20];
+	printf("\nEnter any string : ");
+	gets(string);
+	printf("\nCharacter string is : ");
+	puts(string);
+	
+    return 0;
+}
+
