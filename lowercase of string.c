0f173e8eaeccd4fc445b5b6a7b56a751fcfa6706
SAHARSH VIJAYRAO KURHADE <95751747+saharshkurhade664@users.noreply.github.com>
2021-12-28 12:25:57 +0530

    Create lowercase of string.c

---
+++ lowercase of string.c
@@ -0,0 +1,11 @@
+# C-Practice-Programs
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[]={'S','A','H','A','R','S','H'};
+	printf(" The Name is \n %s",str);
+	printf(" \n The Lower case of name is \n %s",strlwr(str));
+	return 0;
+}
