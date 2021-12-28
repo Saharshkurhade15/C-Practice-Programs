1807598877cb3bdaf289f97f6747994502801e38
SAHARSH VIJAYRAO KURHADE <95751747+saharshkurhade664@users.noreply.github.com>
2021-12-28 12:37:56 +0530

    Create Upper case of string.c

---
+++ Upper case of string.c
@@ -0,0 +1,11 @@
+# C-Practice-Programs
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[]={'s','a','h','a','r','s','h'};
+	printf(" The Name is \n %s",str);
+	printf(" \n The Upper case of name is \n %s",strupr(str));
+	return 0;
+}
