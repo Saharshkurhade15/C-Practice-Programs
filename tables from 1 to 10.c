de30326d4a46fa664739d7d16faaf2f63c8117c5
SAHARSH VIJAYRAO KURHADE <95751747+saharshkurhade664@users.noreply.github.com>
2021-12-28 12:18:47 +0530

    Create tables from 1 to 10.c

---
+++ tables from 1 to 10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		printf("\n%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t",1*i,2*i,3*i,4*i,5*i,6*i,7*i,8*i,9*i,10*i);
+	}
+	return 0;
+}
